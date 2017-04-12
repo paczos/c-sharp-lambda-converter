129c6bbd7af3bba0ea444cfd381e0783cd885c48
paczopaczos <pawel.paczuski@hotmail.com>
2017-04-12 20:51:48 +0200

    listing lambdas in source file

---
+++ lambda_converter/lambda_converter/Program.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace lambda_converter
+{
+
+    class Program
+    {
+
+        static bool IsLambda(SyntaxNode node)
+        {
+            switch (node.Kind())
+            {
+                case SyntaxKind.ParenthesizedLambdaExpression:
+                case SyntaxKind.SimpleLambdaExpression:
+                case SyntaxKind.AnonymousMethodExpression:
+                case SyntaxKind.LetClause:
+                case SyntaxKind.WhereClause:
+                case SyntaxKind.AscendingOrdering:
+                case SyntaxKind.DescendingOrdering:
+                case SyntaxKind.JoinClause:
+                case SyntaxKind.GroupClause:
+                case SyntaxKind.LocalFunctionStatement:
+                    return true;
+
+                case SyntaxKind.FromClause:
+                    // The first from clause of a query expression is not a lambda.
+                    return !node.Parent.IsKind(SyntaxKind.QueryExpression);
+            }
+
+            return false;
+        }
+
+        static string CODELOCATION = ".\\target_code\\LambdaCode.cs";
+        static int Main(string[] args)
+        {
+            string code;
+
+            using (StreamReader sr = new StreamReader(CODELOCATION))
+            {
+                code = sr.ReadToEnd();
+
+            }
+
+            var tree = CSharpSyntaxTree.ParseText(code);
+            var root = tree.GetRoot();
+
+            var lambdas = root.DescendantNodes().Where(m => IsLambda(m) == true);
+            
+            foreach( var l in lambdas)
+            {
+                Console.WriteLine(l.ToString());
+            }
+
+            return 0;
+        }
+
+        private static IEnumerable<object> SimpleLambdaExpression()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ lambda_converter/lambda_converter/target_code/LambdaCode.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lambda_converter.target_code
+{
+    class LambdaCode
+    {
+        List<int> ints = new List<int> { 1, 356, 23, 1, 56, 2, 123, 555, 78, 221, 4, 0, 2, 5, 1 };
+
+       public void Method()
+        {
+            
+            var even = ints.Where(m => m % 2 == 0).ToList();
+
+            int[] externalInts = { 1, 3, 5 };
+            int[] localInts = { 3, 6, 9 };
+
+            //expression lambda
+            var zipped = localInts.Zip(externalInts, (m, n)=>m-n).ToList();
+
+            string text = "result of zipping";
+
+            //statement lamda with capture
+            zipped.ForEach(n => {
+
+                Console.WriteLine(text);
+                Console.WriteLine(n);
+            });
+
+            Func<float, float> lam = o => o - 1.3f;
+            lam(5.0f);
+
+            //Func<int> voidLam = () => 3;
+            //nested lambda
+            Func<int,Func<int>> nested = (b) => () => b*3;
+
+            //recursive lambda
+            Func<Func<int, int>, Func<int, int>> factorial = (fac) => x => x == 0 ? 0 : x * fac(x - 1); 
+            
+
+        }
+
+    }
+}
